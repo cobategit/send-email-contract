555a2cea9f05a2f743f9b5d4975b1329472fcaf3
cobategit <mancunianmerchant816@gmail.com>
2023-02-23 09:04:48 +0700

    scheduled

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { taskWeekly } = require('./services/weekly/task.js')
 const { taskWeeklyCurah } = require('./services/weekly-curah/task.js')
 
-taskWeekly.start()
 taskWeeklyCurah.start()
+taskWeekly.start()
