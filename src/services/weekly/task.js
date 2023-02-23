1122b989d25988b099797d36758b454991f3b0c1
cobategit <mancunianmerchant816@gmail.com>
2023-02-23 09:02:10 +0700

    scheduled

--- src/services/weekly/task.js
+++ src/services/weekly/task.js
@@ -3,7 +3,7 @@ const shelljs = require('shelljs')
 const path = require('path')
 
 const taskWeekly = cronNode.schedule(
-  `15 6 * * *`,
+  `10 7 * * *`,
   () => {
     console.log(`schedule weekly running...`)
     if (shelljs.exec(`node ${path.join(__dirname, 'index.js')}`).code !== 0) {
