010ef2dbdde766d6c00adf97b5ea6585d2a4ffaa
cobategit <mancunianmerchant816@gmail.com>
2023-02-27 09:17:37 +0700

    task

--- src/services/weekly-curah/task.js
+++ src/services/weekly-curah/task.js
@@ -5,13 +5,15 @@ const path = require('path')
 const taskWeeklyCurah = cronNode.schedule(
   `10 6 * * *`,
   () => {
-    console.log(`schedule weekly curah running...`)
     if (shelljs.exec(`node ${path.join(__dirname, 'index.js')}`).code !== 0) {
       console.log(`terjadi kesalahan cron job weekly curah`)
     }
+    console.log(`schedule weekly curah running...`)
   },
   { scheduled: true, timezone: 'Asia/Jakarta' }
 )
+
+taskWeeklyCurah.start()
 
 module.exports = {
   taskWeeklyCurah,
--- src/services/weekly/task.js
+++ src/services/weekly/task.js
@@ -5,13 +5,15 @@ const path = require('path')
 const taskWeekly = cronNode.schedule(
   `10 7 * * *`,
   () => {
-    console.log(`schedule weekly running...`)
     if (shelljs.exec(`node ${path.join(__dirname, 'index.js')}`).code !== 0) {
       console.log(`terjadi kesalahan cron job weekly`)
     }
+    console.log(`schedule weekly running...`)
   },
   { scheduled: true, timezone: 'Asia/Jakarta' }
 )
+
+taskWeekly.start()
 
 module.exports = {
   taskWeekly,
